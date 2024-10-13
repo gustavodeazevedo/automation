3a35440ae7cbdf1eb4b5b4a8499326e3ec01bd1d
Gustavo de Azevedo <gustavodeazevedo2003@outlook.com>
2024-10-12 23:25:18 -0300

    init: Starting

---
+++ pratica.py
@@ -0,0 +1,13 @@
+import pyautogui
+import time
+
+time.sleep(1)
+
+pyautogui.press("win")
+
+pyautogui.write("edge")
+
+pyautogui.press("enter")
+
+time.sleep(1)
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
